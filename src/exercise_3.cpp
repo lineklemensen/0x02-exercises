b2936d847c0e564278b91f1112d5f52c8c194389
Line Klemensen <202407497@post.au.dk>
2024-11-11 17:33:45 +0100

    Run and done!

--- src/exercise_3.cpp
+++ src/exercise_3.cpp
@@ -5,6 +5,20 @@ given integer input. Use a while-loop for iterating the integers.*/
 using namespace std;
 
 int main() {
+    int number;
+    int i = 0;
 
+    cout << "Please enter a number: ";
+    cin >> number;
+
+    while (i <= number)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << endl;
+        }
+        i++;
+    }
+    
     return 0;
 }
