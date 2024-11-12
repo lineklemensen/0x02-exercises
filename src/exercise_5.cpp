771ba1ebce801e01ddee6dfd71c59751794f60ab
Line Klemensen <202407497@post.au.dk>
2024-11-12 10:33:22 +0100

    Function to calculate mean, and user-input in int main function

--- src/exercise_5.cpp
+++ src/exercise_5.cpp
@@ -11,6 +11,7 @@ Make the mean calculation into a function*/
 using namespace std;
 
 
+// Function to simulate a die roll
 int rollDie() {
     //Random number generator
     random_device rd; // Seed to the random number engine
@@ -19,8 +20,27 @@ int rollDie() {
     return dist(gen);
 }
 
+// Function to calculate the mean of a vector
+double calculateMean (const vector<int>& rolls) {
+    // Calculate the sum of all throws
+    int sum = accumulate(rolls.begin(), rolls.end(), 0);
+    //Divide the sum by the number of throws to calculate the mean
+    return static_cast<double>(sum) / rolls.size();
+}
+
 int main() {
     int throws;
+
+    cout << "Enter the number of die throws: ";
+    cin >> throws;
+
+    // Vector to store the die rolls
+    vector<int> rolls;
+
+    // Execute the die roll simulation and store the results in the vector
+    for (int i = 0; i < throws; i++) {
+        rolls.push_back(rollDie());
+    }
 
     return 0;
 }
