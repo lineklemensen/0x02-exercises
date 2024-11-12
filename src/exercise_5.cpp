3707d7b54e194fd8597b1b36b73faf0bd65ffc49
Line Klemensen <202407497@post.au.dk>
2024-11-12 10:18:34 +0100

    rollDie function made primarily with help from chat GPT and a little bit from Copilot

--- src/exercise_5.cpp
+++ src/exercise_5.cpp
@@ -10,6 +10,14 @@ Make the mean calculation into a function*/
 using namespace std;
 
 
+int rollDie() {
+    //Random number generator
+    random_device rd; // Seed to the random number engine
+    mt19937 gen(rd()); // Mersenne Twister 19937 generator
+    uniform_int_distribution<> dist(1, 6); // Interval of possible numbers on a die
+    return dist(gen);
+}
+
 int main() {
     int throws;
 
